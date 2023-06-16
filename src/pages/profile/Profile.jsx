9f5cc9f95977c02249c91f68f1ce1eb2522cbcae
German Figueroa <94080266+germanfaob@users.noreply.github.com>
2023-06-16 17:47:37 +0200

    profile fix

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,34 +1,62 @@
 import "./profile.css";
-import { FaHome, FaLinkedinIn, FaRegFileAlt, FaGithub } from "react-icons/fa"
+import {
+  FaHome,
+  FaLinkedinIn,
+  FaRegFileAlt,
+  FaGithub,
+  FaDashcube,
+} from "react-icons/fa";
 
 export const Profile = () => {
   return (
     <div className="profile">
       <h1 className="profile__title">Perfil de usuario</h1>
       <div className="profile__description">
-        <div className="profile__description-img"></div>
+        <div className="profile__description-img">
+          <FaDashcube className="profile__description-icon" />
+        </div>
         <h2 className="profile__description-title">German Figueroa</h2>
         <p className="profile__description-text">Descubre más en:</p>
         <div className="profile__links">
           <ul className="profile__links-list">
             <li className="profile__links-item">
-              <a className="profile__links-a" href="https://germadev.com/" target="_blank">
-                <FaHome className="profile__links-icon"/>
+              <a
+                className="profile__links-a"
+                href="https://germadev.com/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FaHome className="profile__links-icon" />
               </a>
             </li>
             <li className="profile__links-item">
-              <a className="profile__links-a" href="https://www.linkedin.com/in/germanfig/" target="_blank">
-                <FaLinkedinIn className="profile__links-icon"/>
+              <a
+                className="profile__links-a"
+                href="https://www.linkedin.com/in/germanfig/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FaLinkedinIn className="profile__links-icon" />
               </a>
             </li>
             <li className="profile__links-item">
-              <a className="profile__links-a" href="https://github.com/germanfaob" target="_blank">
-                <FaGithub className="profile__links-icon"/>
+              <a
+                className="profile__links-a"
+                href="https://github.com/germanfaob"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FaGithub className="profile__links-icon" />
               </a>
             </li>
             <li className="profile__links-item">
-              <a className="profile__links-a" href="../../resume/resume.pdf" target="_blank">
-                <FaRegFileAlt className="profile__links-icon"/>
+              <a
+                className="profile__links-a"
+                href="../../resume/resume.pdf"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FaRegFileAlt className="profile__links-icon" />
               </a>
             </li>
           </ul>
